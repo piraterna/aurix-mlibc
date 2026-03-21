e31281476e999963f09a9c25f7ad3528e7160b81
Kevin Alavik <kevin@alavik.se>
2026-03-21 09:23:27 +0100

    feat: added basic syscalls

---
+++ sysdeps/aurix/include/sys/syscall_nums.h
@@ -0,0 +1,10 @@
+#ifndef _SYS_SYSCALL_NUMS_H
+#define _SYS_SYSCALL_NUMS_H
+
+#define SYS_EXIT       0
+#define SYS_OPEN       1
+#define SYS_READ       2
+#define SYS_WRITE      3
+#define SYS_CLOSE      4
+
+#endif /* _SYS_SYSCALL_NUMS_H */
