a0c253ad9c99c5df188a03514688ac8152b33cd3
Kevin Alavik <kevin@alavik.se>
2026-03-21 20:54:46 +0100

    move back to syscall and register

--- sysdeps/aurix/generic/syscall.cpp
+++ sysdeps/aurix/generic/syscall.cpp
@@ -7,91 +7,105 @@ extern "C" long __do_syscall_ret(unsigned long ret) {
 }
 
 sc_word_t __do_syscall0(long sc) {
-	sc_word_t ret;
+	register sc_word_t rax asm("rax") = (sc_word_t)sc;
 	asm volatile (
-		"int $0x80"
-		: "=a"(ret)
-		: "a"(sc)
+		"syscall"
+		: "+r"(rax)
+		:
 		: "rcx", "r11", "memory"
 	);
-	return ret;
+	return rax;
 }
 
 sc_word_t __do_syscall1(long sc, sc_word_t arg1) {
-	sc_word_t ret;
+	register sc_word_t rax asm("rax") = (sc_word_t)sc;
+	register sc_word_t rdi asm("rdi") = arg1;
 	asm volatile (
-		"int $0x80"
-		: "=a"(ret)
-		: "a"(sc), "D"(arg1)
+		"syscall"
+		: "+r"(rax)
+		: "r"(rdi)
 		: "rcx", "r11", "memory"
 	);
-	return ret;
+	return rax;
 }
 
 sc_word_t __do_syscall2(long sc, sc_word_t arg1, sc_word_t arg2) {
-	sc_word_t ret;
+	register sc_word_t rax asm("rax") = (sc_word_t)sc;
+	register sc_word_t rdi asm("rdi") = arg1;
+	register sc_word_t rsi asm("rsi") = arg2;
 	asm volatile (
-		"int $0x80"
-		: "=a"(ret)
-		: "a"(sc), "D"(arg1), "S"(arg2)
+		"syscall"
+		: "+r"(rax)
+		: "r"(rdi), "r"(rsi)
 		: "rcx", "r11", "memory"
 	);
-	return ret;
+	return rax;
 }
 
 sc_word_t __do_syscall3(long sc, sc_word_t arg1, sc_word_t arg2, sc_word_t arg3) {
-	sc_word_t ret;
+	register sc_word_t rax asm("rax") = (sc_word_t)sc;
+	register sc_word_t rdi asm("rdi") = arg1;
+	register sc_word_t rsi asm("rsi") = arg2;
+	register sc_word_t rdx asm("rdx") = arg3;
 	asm volatile (
-		"int $0x80"
-		: "=a"(ret)
-		: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3)
+		"syscall"
+		: "+r"(rax)
+		: "r"(rdi), "r"(rsi), "r"(rdx)
 		: "rcx", "r11", "memory"
 	);
-	return ret;
+	return rax;
 }
 
 sc_word_t __do_syscall4(long sc,
 		sc_word_t arg1, sc_word_t arg2, sc_word_t arg3,
 		sc_word_t arg4) {
-	sc_word_t ret;
+	register sc_word_t rax asm("rax") = (sc_word_t)sc;
+	register sc_word_t rdi asm("rdi") = arg1;
+	register sc_word_t rsi asm("rsi") = arg2;
+	register sc_word_t rdx asm("rdx") = arg3;
+	register sc_word_t r10 asm("r10") = arg4;
 	asm volatile (
-		"mov %5, %%r10\n\t"
-		"int $0x80"
-		: "=a"(ret)
-		: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3), "r"(arg4)
-		: "r10", "rcx", "r11", "memory"
+		"syscall"
+		: "+r"(rax)
+		: "r"(rdi), "r"(rsi), "r"(rdx), "r"(r10)
+		: "rcx", "r11", "memory"
 	);
-	return ret;
+	return rax;
 }
 
 sc_word_t __do_syscall5(long sc,
 		sc_word_t arg1, sc_word_t arg2, sc_word_t arg3,
 		sc_word_t arg4, sc_word_t arg5) {
-	sc_word_t ret;
+	register sc_word_t rax asm("rax") = (sc_word_t)sc;
+	register sc_word_t rdi asm("rdi") = arg1;
+	register sc_word_t rsi asm("rsi") = arg2;
+	register sc_word_t rdx asm("rdx") = arg3;
+	register sc_word_t r10 asm("r10") = arg4;
+	register sc_word_t r8  asm("r8")  = arg5;
 	asm volatile (
-		"mov %5, %%r10\n\t"
-		"mov %6, %%r8\n\t"
-		"int $0x80"
-		: "=a"(ret)
-		: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3), "r"(arg4), "r"(arg5)
-		: "r8", "r10", "rcx", "r11", "memory"
+		"syscall"
+		: "+r"(rax)
+		: "r"(rdi), "r"(rsi), "r"(rdx), "r"(r10), "r"(r8)
+		: "rcx", "r11", "memory"
 	);
-	return ret;
+	return rax;
 }
 
 sc_word_t __do_syscall6(long sc,
 		sc_word_t arg1, sc_word_t arg2, sc_word_t arg3,
 		sc_word_t arg4, sc_word_t arg5, sc_word_t arg6) {
-	sc_word_t ret;
+	register sc_word_t rax asm("rax") = (sc_word_t)sc;
+	register sc_word_t rdi asm("rdi") = arg1;
+	register sc_word_t rsi asm("rsi") = arg2;
+	register sc_word_t rdx asm("rdx") = arg3;
+	register sc_word_t r10 asm("r10") = arg4;
+	register sc_word_t r8  asm("r8")  = arg5;
+	register sc_word_t r9  asm("r9")  = arg6;
 	asm volatile (
-		"mov %5, %%r10\n\t"
-		"mov %6, %%r8\n\t"
-		"mov %7, %%r9\n\t"
-		"int $0x80"
-		: "=a"(ret)
-		: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3),
-		  "r"(arg4), "r"(arg5), "r"(arg6)
-		: "r8", "r9", "r10", "rcx", "r11", "memory"
+		"syscall"
+		: "+r"(rax)
+		: "r"(rdi), "r"(rsi), "r"(rdx), "r"(r10), "r"(r8), "r"(r9)
+		: "rcx", "r11", "memory"
 	);
-	return ret;
+	return rax;
 }
