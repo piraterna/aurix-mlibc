b45c1719237d39e17aa993c4bb008cf9ed49f8b9
Kevin Alavik <kevin@alavik.se>
2026-03-21 20:51:45 +0100

    test

--- sysdeps/aurix/generic/syscall.cpp
+++ sysdeps/aurix/generic/syscall.cpp
@@ -8,36 +8,45 @@ extern "C" long __do_syscall_ret(unsigned long ret) {
 
 sc_word_t __do_syscall0(long sc) {
 	sc_word_t ret;
-	asm volatile ("int $0x80" : "=a"(ret)
-			: "a"(sc)
-			: "rcx", "r11", "memory");
+	asm volatile (
+		"int $0x80"
+		: "=a"(ret)
+		: "a"(sc)
+		: "rcx", "r11", "memory"
+	);
 	return ret;
 }
 
-sc_word_t __do_syscall1(long sc,
-		sc_word_t arg1) {
+sc_word_t __do_syscall1(long sc, sc_word_t arg1) {
 	sc_word_t ret;
-	asm volatile ("int $0x80" : "=a"(ret)
-			: "a"(sc), "D"(arg1)
-			: "rcx", "r11", "memory");
+	asm volatile (
+		"int $0x80"
+		: "=a"(ret)
+		: "a"(sc), "D"(arg1)
+		: "rcx", "r11", "memory"
+	);
 	return ret;
 }
 
-sc_word_t __do_syscall2(long sc,
-		sc_word_t arg1, sc_word_t arg2) {
+sc_word_t __do_syscall2(long sc, sc_word_t arg1, sc_word_t arg2) {
 	sc_word_t ret;
-	asm volatile ("int $0x80" : "=a"(ret)
-			: "a"(sc), "D"(arg1), "S"(arg2)
-			: "rcx", "r11", "memory");
+	asm volatile (
+		"int $0x80"
+		: "=a"(ret)
+		: "a"(sc), "D"(arg1), "S"(arg2)
+		: "rcx", "r11", "memory"
+	);
 	return ret;
 }
 
-sc_word_t __do_syscall3(long sc,
-		sc_word_t arg1, sc_word_t arg2, sc_word_t arg3) {
+sc_word_t __do_syscall3(long sc, sc_word_t arg1, sc_word_t arg2, sc_word_t arg3) {
 	sc_word_t ret;
-	asm volatile ("int $0x80" : "=a"(ret)
-			: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3)
-			: "rcx", "r11", "memory");
+	asm volatile (
+		"int $0x80"
+		: "=a"(ret)
+		: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3)
+		: "rcx", "r11", "memory"
+	);
 	return ret;
 }
 
@@ -45,11 +54,13 @@ sc_word_t __do_syscall4(long sc,
 		sc_word_t arg1, sc_word_t arg2, sc_word_t arg3,
 		sc_word_t arg4) {
 	sc_word_t ret;
-	register sc_word_t arg4_reg asm("r10") = arg4;
-	asm volatile ("int $0x80" : "=a"(ret)
-			: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3),
-				"r"(arg4_reg)
-			: "rcx", "r11", "memory");
+	asm volatile (
+		"mov %5, %%r10\n\t"
+		"int $0x80"
+		: "=a"(ret)
+		: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3), "r"(arg4)
+		: "r10", "rcx", "r11", "memory"
+	);
 	return ret;
 }
 
@@ -57,12 +68,14 @@ sc_word_t __do_syscall5(long sc,
 		sc_word_t arg1, sc_word_t arg2, sc_word_t arg3,
 		sc_word_t arg4, sc_word_t arg5) {
 	sc_word_t ret;
-	register sc_word_t arg4_reg asm("r10") = arg4;
-	register sc_word_t arg5_reg asm("r8") = arg5;
-	asm volatile ("int $0x80" : "=a"(ret)
-			: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3),
-				"r"(arg4_reg), "r"(arg5_reg)
-			: "rcx", "r11", "memory");
+	asm volatile (
+		"mov %5, %%r10\n\t"
+		"mov %6, %%r8\n\t"
+		"int $0x80"
+		: "=a"(ret)
+		: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3), "r"(arg4), "r"(arg5)
+		: "r8", "r10", "rcx", "r11", "memory"
+	);
 	return ret;
 }
 
@@ -70,13 +83,15 @@ sc_word_t __do_syscall6(long sc,
 		sc_word_t arg1, sc_word_t arg2, sc_word_t arg3,
 		sc_word_t arg4, sc_word_t arg5, sc_word_t arg6) {
 	sc_word_t ret;
-	register sc_word_t arg4_reg asm("r10") = arg4;
-	register sc_word_t arg5_reg asm("r8") = arg5;
-	register sc_word_t arg6_reg asm("r9") = arg6;
-	asm volatile ("int $0x80" : "=a"(ret)
-			: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3),
-				"r"(arg4_reg), "r"(arg5_reg), "r"(arg6_reg)
-			: "rcx", "r11", "memory");
+	asm volatile (
+		"mov %5, %%r10\n\t"
+		"mov %6, %%r8\n\t"
+		"mov %7, %%r9\n\t"
+		"int $0x80"
+		: "=a"(ret)
+		: "a"(sc), "D"(arg1), "S"(arg2), "d"(arg3),
+		  "r"(arg4), "r"(arg5), "r"(arg6)
+		: "r8", "r9", "r10", "rcx", "r11", "memory"
+	);
 	return ret;
 }
-
